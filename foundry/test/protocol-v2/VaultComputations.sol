0425c79560397322b50d350f5f62164e3821a05f
blackwhale.eth <victor.blackwhale@gmail.com>
2025-08-28 17:57:33 +0200

    feat (tests): create first vault test base file + add filtered test cmd

---
+++ foundry/test/protocol-v2/VaultComputations.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+// Fixtures
+import { Fixtures } from "./fixtures.sol";
+// Contracts
+import { ILedgityYieldVault } from "src/protocol-v2/interfaces/ILedgityYieldVault.sol";
+import { ILedgityDataProvider } from "src/protocol-v2/interfaces/ILedgityDataProvider.sol";
+
+contract VaultComputationsTest is Fixtures {
+  function setUp() public {
+    _setUp();
+  }
+}
