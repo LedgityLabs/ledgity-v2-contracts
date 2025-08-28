a15132c11748f7be470ac543c3bd69d5e52a6b41
blackwhale.eth <victor.blackwhale@gmail.com>
2025-08-28 17:57:02 +0200

    feat (tests): create fixtures for testing env

---
+++ foundry/test/protocol-v2/Fixtures.sol
@@ -0,0 +1,290 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Foundry
+import "../../lib/forge-std/src/Test.sol";
+
+// v2 Contracts
+import { LedgityYieldVault } from "../../../src/protocol-v2/LedgityYieldVault.sol";
+// v1 Contracts
+import { GlobalOwner } from "../../../src/protocol-v1/GlobalOwner.sol";
+import { GlobalPause } from "../../../src/protocol-v1/GlobalPause.sol";
+import { GlobalBlacklist } from "../../../src/protocol-v1/GlobalBlacklist.sol";
+import { GenericERC20 } from "../../../src/protocol-v1/GenericERC20.sol";
+import { LDYStaking } from "../../../src/protocol-v1/LDYStaking.sol";
+// Contracts
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+// Mock Contracts
+import { MockLToken } from "../../../src/protocol-v1/mock/MockLToken.sol";
+import { MockERC20 } from "../../../src/protocol-v1/mock/MockERC20.sol";
+// Interfaces
+import { ILedgityYieldVault } from "../../../src/protocol-v2/interfaces/ILedgityYieldVault.sol";
+import { IVaultLiquidityModule } from "../../../src/protocol-v2/interfaces/IVaultLiquidityModule.sol";
+
+contract Fixtures is Test {
+  // ======== LIBS ======== //
+  using Strings for string;
+
+  // ======== STORAGE ======== //
+  uint256 private checkpointSnapshotInitial;
+  uint256 private checkpointSnapshot;
+
+  // ======== CONFIGS
+
+  LDYStaking.StakeDurationInfo[] public stakingDurationInfos;
+  uint256[] durations = [0, 1, 6, 12, 24, 36];
+
+  // ======== CONTRACTS
+
+  IERC20 usdc;
+  IERC20 weth;
+  MockERC20 mockUsdc;
+  MockERC20 mockWeth;
+
+  GlobalOwner globalOwner;
+  GlobalPause globalPause;
+  GlobalBlacklist globalBlacklist;
+
+  GenericERC20 ldyToken;
+  LDYStaking ldyStaking;
+
+  // ======== USERS
+
+  address alice = address(0xA11CE);
+  address bob = address(0xB0B);
+  address carol = address(0xCA401);
+  address deployer = address(this);
+
+  address[] users = [alice, bob, carol];
+
+  address feeRecipient = address(feeRecipient);
+  address liquidityManager = address(liquidityManager);
+
+  IAaveLendingPoolV3 aaveLendingPool;
+
+  // ======== CONSTANTS
+
+  uint256 public constant INITIAL_BALANCE = 1_000_000 ether;
+  uint256 public constant RATE_BASE = 100_000;
+  uint256 public constant RAY = 1e27;
+  uint256 public constant APR_RATE_OFFSET = RAY / RATE_BASE;
+
+  // ======== SETUP FUNCTIONS ======== //
+
+  function _setUp() internal {
+    // Expensive setup
+    _deployContracts();
+    _setupInitialState();
+
+    // Save snapshot after expensive setup
+    checkpointSnapshotInitial = vm.snapshot();
+  }
+
+  function _deployContracts() private {
+    for (uint256 i = 0; i < durations.length; i++) {
+      stakingDurationInfos.push(
+        LDYStaking.StakeDurationInfo(durations[i] * OneMonth, 10000)
+      );
+    }
+
+    // Deploy token
+    usdc = _getUsdcToken();
+    weth = _getWethToken();
+    mockUsdc = new MockERC20("Mock USDC", "mUSDC", 6);
+    mockWeth = new MockERC20("Mock WETH", "mWETH", 18);
+    ldyToken = new GenericERC20("Ledgity Token", "LDY", 18);
+
+    aaveLendingPool = _getAaveV3LendingPool();
+
+    GlobalOwner globalOwnerImpl = new GlobalOwner();
+    GlobalPause globalPauseImpl = new GlobalPause();
+    GlobalBlacklist globalBlacklistImpl = new GlobalBlacklist();
+    LDYStaking ldyStakingImpl = new LDYStaking();
+
+    // Deploy proxies
+    ERC1967Proxy globalOwnerProxy = new ERC1967Proxy(
+      address(globalOwnerImpl),
+      ""
+    );
+    ERC1967Proxy globalPauseProxy = new ERC1967Proxy(
+      address(globalPauseImpl),
+      ""
+    );
+    ERC1967Proxy globalBlacklistProxy = new ERC1967Proxy(
+      address(globalBlacklistImpl),
+      ""
+    );
+    ERC1967Proxy ldyStakingProxy = new ERC1967Proxy(
+      address(ldyStakingImpl),
+      ""
+    );
+
+    globalOwner = GlobalOwner(address(globalOwnerProxy));
+    globalPause = GlobalPause(address(globalPauseProxy));
+    globalBlacklist = GlobalBlacklist(address(globalBlacklistProxy));
+    ldyStaking = LDYStaking(address(ldyStakingProxy));
+
+    // Setup labels
+    vm.label(address(usdc), "USDC token");
+    vm.label(address(ldyToken), "LDY token");
+    vm.label(address(globalOwner), "GlobalOwner");
+    vm.label(address(globalPause), "GlobalPause");
+    vm.label(address(globalBlacklist), "GlobalBlacklist");
+    vm.label(address(ldyStaking), "LDYStaking");
+    //
+    vm.label(alice, "Alice");
+    vm.label(bob, "Bob");
+    vm.label(carol, "Carol");
+    vm.label(deployer, "Deployer");
+    vm.label(feeRecipient, "Fee Recipient");
+    vm.label(liquidityManager, "Liquidity Manager");
+  }
+
+  function _setupInitialState() private {
+    globalOwner.initialize();
+    globalPause.initialize(address(globalOwner));
+    globalBlacklist.initialize(address(globalOwner));
+
+    ldyStaking.initialize(
+      address(globalOwner),
+      address(globalPause),
+      address(globalBlacklist),
+      address(ldyToken),
+      stakingDurationInfos,
+      12 * OneMonth,
+      1000 * 1e18
+    );
+
+    for (uint256 i; i < users.length; i++) {
+      deal(address(usdc), users[i], INITIAL_BALANCE);
+      deal(address(usdc), users[i], INITIAL_BALANCE);
+      mockUsdc.mint(users[i], INITIAL_BALANCE);
+      mockWeth.mint(users[i], INITIAL_BALANCE);
+    }
+  }
+
+  // ======== CHECKPOINT FUNCTIONS ======== //
+
+  function _createCheckpoint() internal {
+    checkpointSnapshot = vm.snapshot();
+  }
+
+  function _revertToCheckpoint() internal {
+    vm.revertTo(checkpointSnapshot);
+  }
+
+  function _revertToCheckpointInitial() internal {
+    vm.revertTo(checkpointSnapshotInitial);
+  }
+
+  // ======== ACTION FUNCTIONS ======== //
+
+  function _createLToken() internal returns (MockLToken) {
+    MockLToken lTokenImpl = new MockLToken();
+    ERC1967Proxy lTokenProxy = new ERC1967Proxy(
+      address(lTokenImpl),
+      ""
+    );
+    MockLToken lToken = MockLToken(address(lTokenProxy));
+
+    string memory name = string("Ledgity " + asset_.symbol());
+    string memory symbol = string("L" + asset_.symbol());
+
+    lToken.initialize(
+      address(globalOwner),
+      address(globalPause),
+      address(globalBlacklist),
+      address(ldyStaking),
+      address(usdc),
+      name,
+      symbol
+    );
+
+    return lToken;
+  }
+
+  function _createVault(
+    IERC20 asset_,
+    address lToken_
+  ) internal returns (LedgityYieldVault) {
+    LedgityYieldVault yieldVaultImpl = new LedgityYieldVault();
+    ERC1967Proxy yieldVaultProxy = new ERC1967Proxy(
+      address(yieldVaultImpl),
+      ""
+    );
+    LedgityYieldVault yieldVault = LedgityYieldVault(
+      address(yieldVaultProxy)
+    );
+
+    string memory name = string("Ledgity " + asset_.name());
+    string memory symbol = string("ly" + asset_.symbol());
+
+    ILedgityYieldVault.VaultParams memory vaultParams = ILedgityYieldVault
+      .VaultParams({
+        name: name,
+        symbol: symbol,
+        asset: address(asset_),
+        lToken: lToken_,
+        stakeToken: address(ldyToken),
+        stakeBalanceForFeeReduction: 1000 * 1e18,
+        globalOwner: address(globalOwner),
+        globalPause: address(globalPause),
+        globalBlacklist: address(globalBlacklist),
+        liquidityManager: address(liquidityManager),
+        feeRecipient: payable(feeRecipient),
+        liquidityBufferRate: 10_000, // 10%
+        aaveLendingPool: aaveLendingPool
+      });
+
+    IVaultLiquidityModule.VaultLiquidityInitParams
+      memory vaultLiquidityInitParams = IVaultLiquidityModule
+        .VaultLiquidityInitParams({
+          highWaterMark: 0,
+          deploymentDelay: 3,
+          yieldAPR: 7_000, // 7%
+          managementFeeRate: 1_000, // 1%
+          performanceFeeRate: 2_000, // 2%
+          withdrawalFeeRate: 500, // 0.5%
+          withdrawalGasFee: 10000000000000000 // 0.01 ETH
+        });
+
+    yieldVault.initialize(vaultParams, vaultLiquidityInitParams);
+
+    return yieldVault;
+  }
+
+  // ======== HELPER FUNCTIONS ======== //
+
+  function _getAaveV3LendingPool()
+    internal
+    view
+    returns (IAaveLendingPoolV3)
+  {
+    if (block.chainid == 1) {
+      // Mainnet
+      return
+        IAaveLendingPoolV3(
+          0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2
+        );
+    } else if (block.chainid == 42161) {
+      // Arbitrum
+      return
+        IAaveLendingPoolV3(
+          0x794a61358D6845594F94dc1DB02A252b5b4814aD
+        );
+    }
+    revert("AaveLendingPool not set");
+  }
+
+  function _getUsdcToken() internal view returns (IERC20) {
+    if (block.chainid == 1) {
+      // Mainnet
+      return IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    } else if (block.chainid == 42161) {
+      // Arbitrum
+      return IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831);
+    }
+    revert("AaveLendingPool not set");
+  }
+}
