db76697d3ffb1037129b7b867e668e0eb3f4b05c
blackwhale.eth <victor.blackwhale@gmail.com>
2025-09-01 14:49:57 +0200

    fix (cmd): repair typings script & wagmi configs + gen typings

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,44 +1,90 @@
 import { defineConfig } from "@wagmi/cli";
 import { hardhat, react, actions } from "@wagmi/cli/plugins";
-import deployments from "./data/deployments.json";
+import deployedContracts from "./data/deployments.json";
 
-// Converts deployments data into format awaited by the hardhat plugin
-// See: https://wagmi.sh/cli/plugins/hardhat#deployments-optional
-// Generic contracts are implementation for proxies
-const genericContracts = [
+type ContractType = {
+  abi: any;
+  address: {
+    [chainId: number]: `0x${string}`;
+  };
+  name: string;
+};
+
+type DeploymentsType = {
+  [name: string]: {
+    [chainId: number]: `0x${string}`;
+  };
+};
+
+/// @dev Implementation contracts or libraries we want to avoid
+const filterList = [
+  "APRHistory",
+  "Multicall3",
+  "USDC",
   "WrappedLToken",
-  "WrappedLTokenSonic",
-  "WrappedLTokenHedera",
-  "LToken",
-  "LTokenSonic",
-  "LTokenHedera",
+  "LTokenSignaler",
 ];
-let hhPluginDeployments = {};
-for (let chainId in deployments) {
-  let contractList =
-    deployments[chainId as keyof typeof deployments][0].contracts;
-  for (const [contractName, contractData] of Object.entries(contractList)) {
-    // Exclude implementation and proxy contracts
-    if (
-      genericContracts.includes(contractName) ||
-      contractName.includes("_Implementation") ||
-      contractName.includes("_Proxy")
-    )
-      continue;
+const contracts: ContractType[] = [];
 
-    if (!hhPluginDeployments[contractName])
-      hhPluginDeployments[contractName] = {};
-    hhPluginDeployments[contractName][chainId] = contractData.address;
+for (const chainId in deployedContracts) {
+  const contractsData = deployedContracts[chainId][0].contracts;
+
+  if (!contractsData) {
+    console.log("No contracts found for chainId: ", chainId);
+    continue;
+  }
+
+  for (const [name, data] of Object.entries(contractsData)) {
+    const chainNumber = Number(chainId);
+    const cleanName = name.replace("_Proxy", "");
+
+    if (filterList.includes(cleanName)) continue;
+    // Exclude implementation
+    if (name.includes("_Implementation")) continue;
+    // Exclude chain specific implementation that have same interfaces
+    if (cleanName.endsWith("Sonic")) continue;
+    if (cleanName.endsWith("Hedera")) continue;
+
+    const foundItem = contracts.find(
+      (item: ContractType) => item.name === cleanName,
+    );
+
+    if (foundItem) {
+      foundItem.address[chainNumber] = (data as any).address;
+    } else {
+      contracts.push({
+        abi: (data as any).abi,
+        address: {
+          [chainNumber]: (data as any).address,
+        },
+        name: cleanName,
+      });
+    }
   }
 }
 
+console.log(
+  "=> Generating typing for: ",
+  JSON.stringify(
+    contracts.map((contract) => contract.name),
+    null,
+    2,
+  ),
+);
+
+const deployments: DeploymentsType = contracts.reduce((acc, contract) => {
+  acc[contract.name] = contract.address;
+  return acc;
+}, {} as DeploymentsType);
+
 export default defineConfig({
-  out: "src/types/contractTypes.ts",
+  out: "types/contractTypes.ts",
+  contracts,
   plugins: [
     hardhat({
-      project: "./",
-      deployments: hhPluginDeployments,
-      include: ["src/"],
+      project: "../ledgity-v2-contracts/",
+      deployments,
+      include: ["src/protocol-v2/"],
       exclude: ["src/protocol-v1/abstracts/**", "src/protocol-v1/libs/**"],
     }),
     react(),
